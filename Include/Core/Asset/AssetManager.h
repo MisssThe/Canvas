110a2a421a78ee1d7844d601195b7f87d2b89003
MisThe <missthe0116@163.com>
2023-06-03 20:08:50 +0800

    新增AssetManager

--- Include/Core/Asset/AssetManager.h
+++ Include/Core/Asset/AssetManager.h
@@ -5,9 +5,64 @@
 #ifndef CANVAS_1_0_ASSETMANAGER_H
 #define CANVAS_1_0_ASSETMANAGER_H
 
+#include <fstream>
+#include "Serialize.h"
+#include "../../General/Container/Map.h"
+#include "../../General/IO.h"
+#include "../../General/Debug.h"
+#include "Asset.h"
 
 class AssetManager {
+private:
+    static Map<std::string, Asset *> *S_assetMap;
+public:
+    struct AssetConfig {
 
+    };
+public:
+    static void S_Config(AssetConfig config);
+    template<class T>
+    static T *S_Create(std::string path) {
+        if (IO::Exist(path)) {
+            Debug::Warn("Asset Create","The Asset Existed [" + path + "]");
+            return S_Instance<T>(path);
+        }
+        T* temp = new T();
+        temp->name = IO::PathToName(path);
+        temp->path = path;
+        S_assetMap->Insert(path, temp);
+        return temp;
+    }
+    template<class T>
+    static T *S_Instance(std::string path) {
+        T *temp = dynamic_cast<T *>(AssetManager::S_assetMap->Get(path));
+        if (temp != nullptr)
+            return temp;
+        std::ifstream is(path);
+        try {
+            if (is.is_open()) {
+                cereal::BinaryInputArchive archive(is);
+                temp = new T();
+                archive(*temp);
+            }
+        } catch (...) {
+            Debug::Warn("Asset Instance", "The Asset Format Does Not Match [" + path + "]");
+        }
+        is.close();
+        if (temp == nullptr)
+            return temp;
+        temp->name = IO::PathToName(path);
+        temp->path = path;
+        S_assetMap->Insert(path, temp);
+        return temp;
+    }
+    static void S_Invoke();
+    static void S_Clear();
+    static void S_Release();
+    static void S_Remove(std::string path);
+    static void S_Remove(Asset *asset);
+private:
+    static void S_Update(Asset *asset);
 };
 
 
--- Source/AssetManager.cpp
+++ Source/AssetManager.cpp
@@ -3,3 +3,47 @@
 //
 
 #include "../Include/Core/Asset/AssetManager.h"
+#include "../Include/Core/GarbageCollection/GarbageCollection.h"
+
+
+Map<std::string, Asset*>* AssetManager::S_assetMap = nullptr;
+
+void AssetManager::S_Config(AssetManager::AssetConfig config) {
+    if (AssetManager::S_assetMap == nullptr)
+        AssetManager::S_assetMap = new Map<std::string, Asset*>();
+    GarbageCollection::S_AddRoot(AssetManager::S_assetMap);
+}
+
+void AssetManager::S_Release() {
+    AssetManager::S_Clear();
+    AssetManager::S_assetMap = nullptr;
+}
+
+void AssetManager::S_Clear() {
+    AssetManager::S_assetMap->Iterator([](std::string path, Asset* asset) {
+        AssetManager::S_Update(asset);
+    });
+    AssetManager::S_assetMap->Clear();
+}
+
+void AssetManager::S_Invoke() {
+
+}
+
+void AssetManager::S_Update(Asset *asset) {
+    Debug::Info("Asset", "Update Asset [" + asset->path + "]");
+    std::ofstream os(asset->path);
+    if (os.is_open()) {
+        cereal::BinaryOutputArchive archive(os);
+        archive(*asset);
+    }
+    os.close();
+}
+
+void AssetManager::S_Remove(std::string path) {
+    Asset* asset = AssetManager::S_assetMap->Get(path);
+    if (asset == nullptr)
+        return;
+    AssetManager::S_assetMap->Remove(path);
+    AssetManager::S_Update(asset);
+}
