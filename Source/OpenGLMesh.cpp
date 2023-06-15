7b711fb76bb2d68ec55d60e0b8eaff6d82e3aad3
MisThe <missthe0116@163.com>
2023-06-16 01:36:49 +0800

    库文件目录调整，OpenGL网格提交完善

--- Source/OpenGLMesh.cpp
+++ Source/OpenGLMesh.cpp
@@ -10,29 +10,52 @@ void OpenGLMesh::CustomMark() {
 }
 
 OpenGLMesh::OpenGLMesh(Mesh *mesh) {
+    mesh->indices = new Vector<unsigned int>{
+            0, 1, 3,
+            1, 2, 3
+    };
+    mesh->vertices = new Vector<float>{
+            0.5f, 0.5f, 0.0f,
+            0.5f, -0.5f, 0.0f,
+            -0.5f, -0.5f, 0.0f,
+            -0.5f, 0.5f, 0.0f,
+    };
+    mesh->colors = new Vector<float>{
+            1.0f, 1.0f, 1.0f, 1.0f,
+            1.0f, 1.0f, 0.0f, 1.0f,
+            1.0f, 0.0f, 1.0f, 1.0f,
+            1.0f, 0.0f, 0.0f, 1.0f,
+//            1.0f, 1.0f, 0.0f,
+//            0.0f, 0.0f, 1.0f,
+//            1.0f, 1.0f, 0.0f,
+    };
+    mesh->uv0s = new Vector<float>{
+            1.0f, 1.0f,
+            1.0f, 0.0f,
+            0.0f, 0.0f,
+            0.0f, 1.0f
+    };
     unsigned int vbo, ebo;
     glGenVertexArrays(1, &this->vao);
     glBindVertexArray(this->vao);
+
+    glGenBuffers(1, &ebo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, mesh->indices->MemorySize(), mesh->indices->Begin(), GL_STATIC_DRAW);
 
     glGenBuffers(1, &vbo);
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
     glBufferData(GL_ARRAY_BUFFER, mesh->Size(), nullptr, GL_STATIC_DRAW);
 
     int offset = 0;
-    offset += this->SubVertexBuffer(offset, mesh->vertices);
-    offset += this->SubVertexBuffer(offset, mesh->normals);
-    offset += this->SubVertexBuffer(offset, mesh->tangents);
-    offset += this->SubVertexBuffer(offset, mesh->colors);
-    offset += this->SubVertexBuffer(offset, mesh->uv0s);
-    offset += this->SubVertexBuffer(offset, mesh->uv1s);
-    offset += this->SubVertexBuffer(offset, mesh->uv2s);
+    offset += this->SubVertexBuffer(offset, mesh->vertices, 0, 3);
+    offset += this->SubVertexBuffer(offset, mesh->normals, 1, 3);
+    offset += this->SubVertexBuffer(offset, mesh->tangents, 2, 4);
+    offset += this->SubVertexBuffer(offset, mesh->colors, 3, 4);
+    offset += this->SubVertexBuffer(offset, mesh->uv0s, 4, 2);
+    offset += this->SubVertexBuffer(offset, mesh->uv1s, 5, 2);
+    offset += this->SubVertexBuffer(offset, mesh->uv2s, 6, 2);
 
-    glGenBuffers(1, &ebo);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, mesh->indices->MemorySize(), mesh->indices->Begin(), GL_STATIC_DRAW);
-
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
 
@@ -54,10 +77,12 @@ OpenGLMesh::~OpenGLMesh() {
     glDeleteVertexArrays(1, &this->vao);
 }
 
-int OpenGLMesh::SubVertexBuffer(int offset, Vector<float> *buffer) {
+int OpenGLMesh::SubVertexBuffer(long int offset, Vector<float> *buffer, int index, int stride) {
     if (buffer == nullptr)
         return 0;
     int size = buffer->MemorySize();
     glBufferSubData(GL_ARRAY_BUFFER,offset,size,buffer->Begin());
+    glVertexAttribPointer(index, stride, GL_FLOAT, GL_FALSE, stride * sizeof(float), (void *) offset);
+    glEnableVertexAttribArray(index);
     return size;
 }
